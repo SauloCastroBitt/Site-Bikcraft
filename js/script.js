ab331248b0d96d37774316b8c5f0398b45995c96
Saulo de Castro Bittencourt <saulocastrobitt@gmail.com>
2021-07-26 15:09:38 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+if(window.SimpleSlide){
+new SimpleSlide ({
+    slide: "quote",
+    auto:true,
+    time: 5000 // significa 5 segundos.
+
+});
+new SimpleSlide ({
+    slide: "portifolio",
+      time: 5000,
+      va: true,
+
+});
+}
+new SimpleAnime({
+    
+})
